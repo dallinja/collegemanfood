9e40e12222d23b1ab841878f4692b602586fc405
Jordan Roper <jordanroper@gmail.com>
2016-04-07 21:03:13 -0600

    Filter feature working. First draft.

--- core/client/scripts/app.js
+++ core/client/scripts/app.js
@@ -1,23 +1,41 @@
 var app = angular
 	.module('cmf', ['ui.router'])
 	.config(function($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/');
+        $urlRouterProvider.otherwise('/home');
         $stateProvider
 	        .state('home', {
-	            url: '/',
 	            templateUrl: 'views/main.html',
 	            controller: 'mainCtrl'
 	        })
+					.state('home.gallery', {
+	            url: '/home',
+							parent:'home',
+	            views:{
+								'dashboard@home':{
+									templateUrl: 'views/homeGallery.html',
+								}
+							}
+	        })
+					.state('home.search', {
+	            url: '/search',
+							parent:'home',
+	            views:{
+								'dashboard@home':{
+									templateUrl: 'views/homeSearch.html',
+									}
+							}
+	        })
+
 	        .state('recipe', {
 	            url: '/recipe/:idNumber',
 	            templateUrl: 'views/recipe.html',
 	            controller: 'recipesCtrl',
 	            resolve: {
-					getRecipe: function(sampleData, $stateParams) {
-						var recipeId = $stateParams.idNumber;
-						return sampleData.getRecipe(recipeId);
-					}
-				}
+								getRecipe: function(sampleData, $stateParams) {
+									var recipeId = $stateParams.idNumber;
+									return sampleData.getRecipe(recipeId);
+								}
+							}
 	        })
 	        .state('login', {
 	            url: '/login',
